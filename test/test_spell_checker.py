545e42c25cd801c7e201fb6e6724bcc6059cdaca
Fariha Syed <fs1688@nyu.edu>
2020-07-06 14:58:14 -0400

    Organized

--- test/test_spell_checker.py
+++ test/test_spell_checker.py
@@ -1,10 +1,10 @@
 import pytest
 from flask import Flask, escape
-import spell_checker
+import application
 
 app = Flask(__name__)
-usernames = ['farihasyed', 'nyu123', 'unc', 'dook', 'unc!!', 'thisusernameislongerthan32characters']
-passwords = ['music', 'nyu123', 'dooksucks', 'uncisawesome', 'dook<sucks>', '!@#$%&*', 'thispasswordislongerthan32characters']
+usernames = ['farihasyed', 'nyu12345', 'uncchapelhill', 'dooksucks', 'unc!!', 'thisusernameislongerthan32characters', 'farihasyed2']
+passwords = ['music922', 'nyu12345', 'dooksucks', 'uncisawesome', 'dook<sucks>', '!@#$%&*', 'thispasswordislongerthan32characters']
 phones = ['9193089764', '1234567890', '2345678910', '9221231995', '123456789100', '123', '0123456789', 'abc123']
 correct_credentials = 3
 credentials = {}
@@ -15,25 +15,28 @@ for i in range(correct_credentials):
 
 @pytest.fixture
 def client():
-    spell_checker.app.config['TESTING'] = True
-    with spell_checker.app.test_client() as client:
-        with spell_checker.app.app_context():
-            s = spell_checker.app
-            c = spell_checker.app.app_context()
+    application.app.config['TESTING'] = True
+    application.app.config['WTF_CSRF_CHECK_DEFAULT'] = False
+    application.app.config['WTF_CSRF_ENABLED'] = False
+    with application.app.test_client() as client:
+        with application.app.app_context():
+            s = application.app
+            c = application.app.app_context()
         yield client
 
 
-def test_spell_check_get_before_log_in(client):
-    response = client.get('/spell_check', follow_redirects=True)
-    assert b'You must be logged in to view this page.' in response.data
-
-
 def register(client, username, password, phone=''):
-    return client.post('/register', data={
-        'uname': username,
-        'pword': password,
-        '2fa': phone
-    }, follow_redirects=True)
+    return client.post('/register', data={'username': username, 'password': password, 'phone': phone},
+                       follow_redirects=True)
+
+
+def login(client, username, password='', phone=''):
+    return client.post('/login', data={'username': username, 'password': password, 'phone': phone},
+                       follow_redirects=True)
+
+
+def spell_check(client, input):
+    return client.post('/spell_check', data={'input': input}, follow_redirects=True)
 
 
 def test_register_get(client):
@@ -61,54 +64,54 @@ def test_register_post(client):
 
     #duplicate uppercase username
     response = register(client=client, username=usernames[0].upper(), password=passwords[3])
-    assert b'Username already taken. Please choose another one.' in response.data
+    assert b'Only lowercase letters and numbers allowed.' in response.data
 
-    #invalid username - not alphanumeric
+    #invalid username - not alphanumeric, too short
     response = register(client=client, username=usernames[4], password=passwords[3])
-    assert b'Invalid username. Must be no longer than 32 characters and only alphanumeric characters allowed.' in response.data
+    assert b'Invalid username.' in response.data
+    assert b'Only lowercase letters and numbers allowed.' in response.data
+    assert b'Must be between 8 and 32 characters long.' in response.data
 
     # invalid username - too long
     response = register(client=client, username=usernames[4], password=passwords[4])
-    assert b'Invalid username. Must be no longer than 32 characters and only alphanumeric characters allowed.' in response.data
+    assert b'Invalid username.' in response.data
+    assert b'Must be between 8 and 32 characters long.' in response.data
 
     #invalid password - has other special characters
     response = register(client=client, username=usernames[2], password=passwords[4])
-    a = escape('Invalid password. Must be no longer than 32 characters and only alphanumeric characters and special characters _, !, @, #, $, %, &, and * allowed.').encode()
-    assert a in response.data
-
-    #invalid password - only special characters
-    response = register(client=client, username=usernames[2], password=passwords[5])
-    a = escape('Invalid password. Must be no longer than 32 characters and only alphanumeric characters and special characters _, !, @, #, $, %, &, and * allowed.').encode()
-    assert a in response.data
+    assert b'Invalid password.' in response.data
+    message = escape('Only letters, numbers, and the following special characters: '
+                     '_, !, @, #, $, %, &, and * allowed.').encode()
+    assert message in response.data
 
     #invalid password - too long
     response = register(client=client, username=usernames[2], password=passwords[5])
-    a = escape('Invalid password. Must be no longer than 32 characters and only alphanumeric characters and special characters _, !, @, #, $, %, &, and * allowed.').encode()
-    assert a in response.data
+    assert b'Invalid password.' in response.data
+    assert b'Must be between 8 and 32 characters long.' in response.data
+
+    # invalid password - only special characters but too short
+    response = register(client=client, username=usernames[2], password=passwords[5])
+    assert b'Invalid password.' in response.data
+    assert b'Must be between 8 and 32 characters long.' in response.data
 
     #invalid phone - too long
     response = register(client=client, username=usernames[2], password=passwords[2], phone=phones[4])
-    assert b'Invalid phone number. Must be of length 10 and only digits allowed.' in response.data
+    assert b'Invalid phone number' in response.data
+    assert b'Only digits allowed and must be 10 digits long.' in response.data
 
     #invalid phone - too short
     response = register(client=client, username=usernames[2], password=passwords[2], phone=phones[5])
-    assert b'Invalid phone number. Must be of length 10 and only digits allowed.' in response.data
+    assert b'Invalid phone number' in response.data
+    assert b'Only digits allowed and must be 10 digits long.' in response.data
 
     #invalid phone - starts with a 0
     response = register(client=client, username=usernames[2], password=passwords[2], phone=phones[6])
-    assert b'Invalid phone number. Must be of length 10 and only digits allowed.' in response.data
+    assert b'Invalid phone number' in response.data
+    assert b'Only digits allowed and must be 10 digits long.' in response.data
 
     #invalid phone - alphanumeric
     response = register(client=client, username=usernames[2], password=passwords[2], phone=phones[7])
-    assert b'Invalid phone number. Must be of length 10 and only digits allowed.' in response.data
-
-
-def login(client, username, password='', phone=''):
-    return client.post('/login', data={
-        'uname': username,
-        'pword': password,
-        '2fa': phone
-    }, follow_redirects=True)
+    assert b'Only digits allowed and must be 10 digits long.' in response.data
 
 
 def test_login_get(client):
@@ -146,18 +149,22 @@ def test_login_post_success(client):
 
 
 def test_login_post_failure(client):
-    register(client, username=usernames[0], password=passwords[0], phone=phones[0])
     #nonregistered user
     response = login(client=client, username=usernames[3], password=passwords[3])
-    assert b'Incorrect username and/or password. Please try again.' in response.data
+    assert b'Incorrect username, password, and/or phone. Please try again.' in response.data
 
     #incorrect password
     response = login(client=client, username=usernames[0], password=passwords[1])
-    assert b'Incorrect username and/or password. Please try again.' in response.data
+    assert b'Incorrect username, password, and/or phone. Please try again.' in response.data
 
     #incorrect phone
     response = login(client=client, username=usernames[0], phone=phones[1])
-    assert b'Two-factor authentication failure' in response.data
+    assert b'Two-factor authentication failure.' in response.data
+
+
+def test_spell_check_get_before_log_in(client):
+    response = client.get('/spell_check', follow_redirects=True)
+    assert b'You must be logged in to view this page.' in response.data
 
 
 def test_spell_check_get_after_log_in(client):
@@ -170,8 +177,8 @@ def test_spell_check_get_after_log_in(client):
 
 
 def test_spell_check_post(client):
-    register(client, username=usernames[0], password=passwords[0], phone=phones[0])
     login(client, username=usernames[0], password=passwords[0], phone=phones[0])
+
     #valid input
     input = 'Take a sad sogn and make it better. Remember to let her under your skyn, then you begin to make it betta.'
     response = spell_check(client, input)
@@ -193,7 +200,7 @@ def test_spell_check_post(client):
     assert ('You are logged in as ' + usernames[0] + '.').encode() in response.data
     assert b'Input text: &amp;lt;&amp;gt;!@#$%!@#$' in response.data
     assert b'The following 1 words were misspelled:' in response.data
-    assert b'amp;lt;&amp;amp;gt' in response.data
+    assert b'lt;&amp;gt' in response.data
 
     #invalid input
     input = '<><><><>>'
@@ -201,19 +208,17 @@ def test_spell_check_post(client):
     assert b'Spell Check' in response.data
     assert b'Enter text to be spell checked.' in response.data
     assert ('You are logged in as ' + usernames[0] + '.').encode() in response.data
+    assert b'Input text: &amp;lt;&amp;gt;&amp;lt;&amp;gt;&amp;lt;&amp;gt;&amp;lt;&amp;gt;&amp;gt;' in response.data
     assert b'Could not spell check invalid input.' in response.data
 
     #input too long
-    input = "i'm trying to overflow the input buffer, which a hacker might do as part of a denial of service (DOS) attack, but i'm a step ahead of them"
+    input = "i'm trying to overflow the input buffer, which a hacker might do as part of a denial of service (DOS) attack, " \
+            "but i'm a step ahead of them"
     response = spell_check(client, input)
     assert b'Spell Check' in response.data
     assert b'Enter text to be spell checked.' in response.data
     assert ('You are logged in as ' + usernames[0] + '.').encode() in response.data
-    assert b'Input too long. Please try again.' in response.data
-
-
-def spell_check(client, input):
-    return client.post('/spell_check', data={'inputtext': input}, follow_redirects=True)
+    assert b'Input cannot be longer than 128 characters' in response.data
 
 
 def test_logout(client):
