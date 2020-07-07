846b307b4c204d1cf5f3243a4b04a9f0c5296c98
Fariha Syed <fs1688@nyu.edu>
2020-07-07 13:49:58 -0400

    Modified in the hopes Travis build will finally pass

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(name='NYU CS9163 Unit 2 Assignment',
       author='Fariha Syed',
       author_email='fs1688@nyu.edu',
       version='1.0',
-      py_modules=['']
+      py_modules=['application']
       )
